bc08900008a8eea8ab776c0c62ec4d1f31d4d908
HoneyBadgerrx <katoyebir@gmail.com>
2022-10-06 11:17:37 -0700

    i am a fool

--- main.c
+++ main.c
@@ -39,9 +39,7 @@ int main(int argc, char *argv[])
 		line_number++;
 		op = strtok(lineptr, "\n\t\r ");
 		if (op != NULL && op[0] != '#')
-		{
 			get_op(op, &stack, line_number);
-		}
 	}
 	exit(EXIT_SUCCESS);
 }
